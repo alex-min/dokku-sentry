5835786a226748fb27a15c14200c27a016d7260b
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-10-09 15:18:06 +0100

    test

--- config/sentry.conf.py
+++ config/sentry.conf.py
@@ -58,8 +58,7 @@ SENTRY_OPTIONS.update({
                 0: {
                     'host': redis_url_parts.hostname,
                     'port': redis_url_parts.port,
-                    'password': redis_url_parts.password,
-                    'username': redis_url_parts.username
+                    'password': redis_url_parts.password
                 },
             },
         },
